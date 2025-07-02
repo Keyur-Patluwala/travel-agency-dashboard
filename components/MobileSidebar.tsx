c4a32b412df903ae460343a72ebe549fefc58ed2
Keyur Patluwala <keyurpatluwala@gmail.com>
2025-07-02 22:58:40 +0530

    added sidebar

---
+++ components/MobileSidebar.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link } from "react-router";
+
+const MobileSidebar = () => {
+  return (
+    <div className="mobile-sidebar wrapper">
+      <header>
+        <Link to="/">
+          <img
+            src="/assets/icons/logo.svg"
+            alt="Logo"
+            className="size-[30px]"
+          />
+        </Link>
+      </header>
+    </div>
+  );
+};
+
+export default MobileSidebar;
